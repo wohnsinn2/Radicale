6973601a9ee1e4657427d7c02bb374aa907a2e3f
Christian Roeder <christian.roeder@red-hood.de>
2015-02-11 02:25:58 +0100

    update http rights module

--- radicale/rights/http.py
+++ radicale/rights/http.py
@@ -14,19 +14,25 @@ log.LOGGER.debug("http rights module loaded")
 
 def get_cal_name(collection):
     # TODO normalize/lowercase?
-    return collection
+    cal_name = collection.rstrip(".ics")
+    cal_name = cal_name.split('/')[-1]
+    return cal_name
 
 
 def _authorized_http(user, collection, permission):
     cal_name = get_cal_name(collection)
     rights_url = Template(RIGHTS_URL_TEMPL).substitute(user=user, cal=cal_name)
-    real_permissions = requests.get(rights_url)
+    response = requests.get(rights_url)
+    state = response.status_code
+    content = response.text or ""
     log.LOGGER.debug("Permission %s reqeusted for user %s on collection %s with"
-                     "calendar name %s with effective permission %s" %  (permission, user, collection, cal_name, real_permissions))
-    return permission in real_permissions
+                     "calendar name %s with effective permission %s" % (permission, user, collection, cal_name, content))
+    return (permission in content) and (state in (200, 201))
 
 
 def authorized(user, collection, permission):
     collection_url = collection.url.rstrip("/") or "/"
-    return True
-    return _authorized_http(user or "", collection_url, permission)
+    if user is None:
+        return False
+    else:
+        return _authorized_http(user, collection_url, permission)
